bbbc04c9859bbc233080cf6855e7bcbe52a8b09e
Faezeh Amri <49558522+amrifaezeh@users.noreply.github.com>
2024-07-09 16:21:21 +0800

    Update login_action_object.py

--- e2e/main_objects/action_objects/login_action_object.py
+++ e2e/main_objects/action_objects/login_action_object.py
@@ -1,7 +1,6 @@
 class validLogin:
     @staticmethod
     def valid_login(sb, email, password):
-        sb.click("a[href='https://www.linkedin.com/login']")
         sb.type("#username", email)
         sb.type("#password", password)
         sb.click("button[aria-label='Sign in']")
